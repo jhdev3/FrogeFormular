6d2ea9f91acab0d5754f000ff11c7da1f0b44558
Jacob Hedén <91886824+jhdev3@users.noreply.github.com>
2022-03-22 09:29:23 +0100

    BaseEntiy
    :+1:

---
+++ FrogeFormular/Models/BaseEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrogeFormular.Models
+{
+    internal class BaseEntity
+    {
+
+        //public int Id { get; set; } //Låta databasen skapa id eller skapa själva? 
+
+        public int Age { get; set; }    
+
+        public bool IsSpanishCar { get; set; }  
+
+        public string CarModels { get; set; }    
+
+
+
+    }
+}
